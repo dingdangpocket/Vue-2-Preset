191c216a59188b7b70e7d5a50a9d6b5cf041a469
niegang <1003018791@qq.com>
2022-07-11 17:07:12 +0800

    save

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,7 +1,20 @@
 <template>
-  <div class="container">
-    <div class="title">
-      中华人民共和国,中华人民共和国,中华人民共和国,中华人民共和国
+  <div>
+    <div class="container">
+      <div class="title">
+        中华人民共和国,中华人民共和国,中华人民共和国,中华人民共和国
+      </div>
+    </div>
+    <div class="goodsContainer">
+      <div class="goodItem" v-for="item in goods" :key="item">
+        {{ item }}
+      </div>
+    </div>
+    <div class="footer">hello,footer</div>
+    <div class="gird">
+      <div class="goodItem" v-for="item in goods" :key="item">
+        {{ item }}
+      </div>
     </div>
   </div>
 </template>
@@ -9,9 +22,10 @@
 <script>
 export default {
   name: "home",
-
   data() {
-    return {};
+    return {
+      goods: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    };
   },
 
   mounted() {},
@@ -29,11 +43,38 @@ export default {
 .container {
   @include flexCenter();
   .title {
-    width: 300px;
+    width: 100vw;
     height: 100px;
-    background: red;
     font-size: 10px;
+    background: red;
     @include flexCenter();
   }
 }
+.goodsContainer {
+  /* overflow-y: auto; */
+  /* width: 100vw; */
+  /* height:300px; */
+  font-size: 10px;
+  display: grid;
+  background: rgb(192, 99, 13);
+  grid-template-columns: repeat(3, 30vw); // 设置列
+  grid-template-rows: repeat(3, 10vw); // 设置行
+  grid-column-gap: 10px; // 设置网格之间的间距
+  grid-row-gap: 10px; // 设置网格之间的间距
+  grid-auto-flow: row;
+  justify-items: center;
+  align-items: center;
+  .goodItem {
+    height: 30px;
+    width: 50px;
+    background: black;
+    color: rgb(255, 255, 255);
+    @include flexCenter();
+  }
+}
+.footer {
+  height: 30px;
+  background: red;
+}
+
 </style>
