5cafcbdf734de3aabbc880d0d748f3c5561e67f2
niegang <1003018791@qq.com>
2022-07-22 23:01:08 +0800

    (feat) 数据传递

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,39 +1,131 @@
 <template>
   <div>
-    <div class="container">
-      <div class="title">
-        测试文本内容
-      </div>
+    <!-- <div class="container">
+      <div class="title">测试文本内容</div>
     </div>
     <div class="goodsContainer">
       <div class="goodItem" v-for="item in goods" :key="item">
         {{ item }}
       </div>
     </div>
-    <div class="footer" v-style="{ color: 'orange', text: 'newText' }">hello,directive</div>
+    <div class="footer" v-style="{ color: 'orange', text: 'newText' }">
+      hello,directive
+    </div> -->
+    <Show
+      :formItems="formItems"
+      firstInterlocks="parkingType"
+      firstInterlocksIndex="2"
+      secondInterlocks="fileType"
+      secondInterlocksIndex="3"
+      thirdInterlocks="areaType"
+      thirdInterlocksIndex="4"
+    />
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import Show from "./show/index.vue";
 export default {
   name: "home",
-
+  components: { Show },
   data() {
     return {
       goods: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
       items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      formItems: [
+        {
+          label: "所属区域",
+          prop: "area",
+          type: "input",
+          bind: "ruleForm.area",
+          autocomplete: "off",
+        },
+        {
+          label: "国家",
+          prop: "parkingType",
+          type: "select",
+          options: [
+            { label: "中国", value: "中国" },
+            { label: "美国", value: "美国" },
+            { label: "加拿大", value: "加拿大" },
+          ],
+          bind: "ruleForm.parkingType",
+          autocomplete: "off",
+          interlocksIndex: 1,
+        },
+        {
+          label: "城市",
+          prop: "fileType",
+          type: "select",
+          options: [],
+          bind: "ruleForm.fileType",
+          autocomplete: "off",
+          interlocksIndex: 2,
+        },
+        {
+          label: "区域",
+          prop: "areaType",
+          type: "select",
+          options: [],
+          bind: "ruleForm.areaType",
+          autocomplete: "off",
+          interlocksIndex: 3,
+        },
+      ],
     };
   },
-
   mounted() {
-    console.log("this.sort(good)", this.sort(this.good));
+    // console.log("this.sort(good)", this.sort(this.good));
+    // this.promiseFunctions();
+    // this.getData();
   },
-  
   methods: {
+    async getData() {
+      const res = await axios({
+        url: "https://retoolapi.dev/kQoG06/data", // 请求的服务器地址
+        method: "GET", // 请求类型
+      });
+      if (res) {
+        this.formItems[1].options = res;
+      }
+    },
+    promiseFunctions() {
+      let PromiseRef = new Promise((resolve) => {
+        setTimeout(() => {
+          resolve("异步线程任务(2)");
+        }, 2000);
+      });
+
+      let PromiseRefOther = new Promise((resolve) => {
+        setTimeout(() => {
+          resolve("异步线程任务(3)");
+        }, 2000);
+      });
+
+      PromiseRef.then(
+        (value) => {
+          console.log("Promise中的一个异步函数的结果输出" + value);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+
+      PromiseRefOther.then(
+        (value) => {
+          console.log("PromiseRefOther中的一个异步函数的结果输出" + value);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+      console.log("同步线程代码(1)");
+    },
     sort(arr) {
       let temp;
-      arr.forEach(() => {
-        arr.forEach((item, k) => {
+      arr?.forEach(() => {
+        arr.forEach((_, k) => {
           if (arr[k] > arr[k + 1]) {
             temp = arr[k];
             arr[k] = arr[k + 1];
---
+++ src/views/home/show/index.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="container">
+    <el-form
+      :model="ruleForm"
+      ref="ruleForm"
+      label-width="100px"
+      class="demo-ruleForm"
+    >
+      <div v-for="item in formItemsState" :key="JSON.stringify(item)">
+        <div v-if="item.type == 'select'">
+          <el-form-item :label="item.label" :prop="item.prop">
+            <el-select v-model="ruleForm[item.prop]">
+              <div v-for="option in item.options" :key="JSON.stringify(option)">
+                <el-option
+                  :label="option.label"
+                  :value="option.value"
+                ></el-option>
+              </div>
+            </el-select>
+          </el-form-item>
+        </div>
+        <div v-if="item.type == 'input'">
+          <el-form-item :label="item.label" :prop="item.prop">
+            <el-input
+              :type="item.type"
+              v-model="ruleForm[item.prop]"
+              :autocomplete="item.autocomplete"
+            ></el-input>
+          </el-form-item>
+        </div>
+      </div>
+      <el-button type="primary" @click="submitForm('ruleForm')"
+        >确 定</el-button
+      >
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "show",
+  props: {
+    formItems: Array,
+    firstInterlocks: String,
+    firstInterlocksIndex: String,
+    secondInterlocks: String,
+    secondInterlocksIndex: String,
+    thirdInterlocks: String,
+    thirdInterlocksIndex: String,
+  },
+  data() {
+    return {
+      ruleForm: {},
+      formItemsState: [],
+    };
+  },
+  mounted() {
+    console.log("PROPS", this.formItems);
+    this.formItemsState = this.formItems;
+    this.ruleForm = { parkingType: "中国", fileType: "海南", areaType: "飞天" };
+  },
+  computed: {
+    firstInterlocksRef() {
+      return this.ruleForm[this.firstInterlocks];
+    },
+    secondInterlocksRef() {
+      return this.ruleForm[this.secondInterlocks];
+    },
+  },
+  watch: {
+    firstInterlocksRef() {
+      const params = this.ruleForm[this.firstInterlocks];
+      console.log(params);
+      if (params == "中国") {
+        this.formItemsState[Number(this.firstInterlocksIndex)].options = [
+          { label: "海南", value: "海南" },
+          { label: "北京", value: "北京" },
+        ];
+        this.ruleForm[this.secondInterlocks] =
+          this.formItemsState[
+            Number(this.firstInterlocksIndex)
+          ].options[0].value;
+      }
+      if (params == "美国") {
+        this.formItemsState[Number(this.firstInterlocksIndex)].options = [
+          { label: "加州", value: "加州" },
+          { label: "洛杉矶", value: "洛杉矶" },
+          { label: "西雅图", value: "西雅图" },
+        ];
+        this.ruleForm[this.secondInterlocks] =
+          this.formItemsState[
+            Number(this.firstInterlocksIndex)
+          ].options[0].value;
+      }
+    },
+
+    secondInterlocksRef() {
+      const params = this.ruleForm[this.secondInterlocks];
+      console.log(params);
+      if (params == "海南") {
+        this.formItemsState[Number(this.secondInterlocksIndex)].options = [
+          { label: "飞天区", value: "飞天区" },
+          { label: "护天区", value: "护天区" },
+        ];
+        this.ruleForm[this.thirdInterlocks] =
+          this.formItemsState[Number(this.secondInterlocksIndex)].options[0].value;
+      }
+      if (params == "北京") {
+        this.formItemsState[Number(this.secondInterlocksIndex)].options = [
+          { label: "朝阳", value: "朝阳" },
+          { label: "五道口", value: "五道口" },
+        ];
+        this.ruleForm[this.thirdInterlocks] =
+          this.formItemsState[Number(this.secondInterlocksIndex)].options[0].value;
+      }
+    },
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((isValid) => {
+        if (isValid) {
+          console.log("保存结果", this.ruleForm);
+        } else {
+          console.log("提交失败");
+          return false;
+        }
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  background: red;
+  width: 200px;
+}
+</style>
